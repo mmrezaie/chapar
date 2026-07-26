fbf18e93cfd4c71d07b4017703d85d21bfd0c7b8
Mohamad Rezaei <mmrezaie@gmail.com>
2026-07-26 04:00:05 +0200

    fix(hpcsim): replace deprecated {dag_hash} format with {hash}
    Spack develop renamed the format attribute from dag_hash to hash. The {dag_hash} spec produces 'Attempted to format callable object'.

--- envs/hpcsim/release.sh
+++ envs/hpcsim/release.sh
@@ -916,8 +916,10 @@ copy_manifest() {
 write_root_module_specs() {
     local output_file="$1"
 
+    # {hash} is the full-length concrete hash in Spack's current format
+    # API. Older Spack used {dag_hash} which was removed in favor of {hash}.
     spack -e "${ENV_PATH}" -C "${BUILD_SCOPE_DIR}" find -c --no-groups \
-        --format "{dag_hash} {name}/{version}" \
+        --format "{hash} {name}/{version}" \
         > "${output_file}"
 }
 
